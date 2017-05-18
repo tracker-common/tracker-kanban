c48c33650544f9cb290b8d2078e05342ee05f05b
Ann Gustafson <anngustafson@mymail.mines.edu>
2017-05-18 14:28:01 -0600

    Nothing really changed but some fiddling around in the js file

--- app/views/welcome/google.js
+++ app/views/welcome/google.js
@@ -1,30 +1,6 @@
-    <head>
-    //Specify client's ID
-        <meta name="google-signin-client_id" content="569976604919-hmipa5tk1gjat8h4k13pviegsuo7e4fe.apps.googleusercontent.com">
-    </head>
-    <body>
-    //Adding button
-    //<div class="g-signin2" data-onsuccess="onSignIn"></div>
-    <div id="my-signin2"></div>
-    <script>
-    function onSuccess(googleUser) {
-	console.log('Logged in as: ' + googleUser.getBasicProfile().getName());
-    }
-function onFailure(error) {
-    console.log(error);
-}
-function renderButton() {
-    gapi.signin2.render('my-signin2', {
-        'scope': 'profile email',
-        'width': 240,
-        'height': 50,
-        'longtitle': true,
-        'theme': 'dark',
-        'onsuccess': onSuccess,
-        'onfailure': onFailure
+function signOut() {
+    var auth2 = gapi.auth2.getAuthInstance();
+    auth2.signOut().then(function () {
+      console.log('User signed out.');
     });
 }
-</script> 
-    //Google platform library
-    <script src="https://apis.google.com/js/platform.js" async defer></script>
-</body>
