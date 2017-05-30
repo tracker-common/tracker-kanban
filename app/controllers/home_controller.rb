be2123c090ca4de6eec94f2addc2b7878b2d7352
Ann Gustafson <anngustafson@mymail.mines.edu>
2017-05-30 12:23:11 -0600

    Signed out page looks good

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def show
   	if session[:user_id] == nil
-  		puts "HELLO WORDL YOU ARE NOT SIGNED IN"
+  		render :layout => 'signed_out'
   	else
+  		
   		render :layout => 'signed_in'
   	end
   end
