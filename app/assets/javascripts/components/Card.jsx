b8f35a54bad7d1f6de39f3b63973ae7836596035
Emily Dederick <edederic@mymail.mines.com>
2017-06-07 23:22:13 -0600

    added icons to indicate story types and adjusted column widths

--- app/assets/javascripts/components/Card.jsx
+++ app/assets/javascripts/components/Card.jsx
@@ -10,8 +10,18 @@ class Card extends React.Component {
           </span>
         );
       }
+  }
+  renderStoryType(){
+    if(this.props.card["story_type"] === "feature") {
+      return (<span className="feature"> </span>);
     }
-
+    if(this.props.card["story_type"] === "bug") {
+      return (<span className="bug"> </span>);
+    }
+    if(this.props.card["story_type"] === "chore") {
+      return (<span className="chore"> </span>);
+    }
+  }
   renderLabel() {
     console.log(this.props.card["labels"].length)
     if (this.props.card["labels"].length != 0){
@@ -25,9 +35,11 @@ class Card extends React.Component {
   render() {
     return (
         <div className="card">
-          <p className="story_name"> {this.props.card["name"]} </p>
+          <p className="story_name">
+            {this.renderStoryType()}
+            {this.props.card["name"]}
+          </p>
           <p className="summary"> {this.props.card["current_state"]} </p>
-          <p className="story_type"> {this.props.card["story_type"]} </p>
           {this.renderLabel()}
           {this.renderUserMessage()}
         </div>
